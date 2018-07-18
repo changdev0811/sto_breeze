147607dace7da12401c27b03262f4be206546a5b
Wade Harkins <wade@ventureinteractive.com>
2018-07-18 14:07:13 -0400

    Top level controller stub `Overseer` added, along with partial implementation of authentication functions from old `homemade.js` in `helper/Auth.js`

---
+++ app/controller/Overseer.js
@@ -0,0 +1,28 @@
+/**
+ * 'Overseeing' application controller. Responsible for checking whether
+ * user is logged in / authorized before rendering, and (possibly) 
+ * lists used models, stores and views.
+ * 
+ * @namespace Breeze.controller
+ * @alias Breeze.controller.Overseer 
+ * @class Overseer
+ * @author wvh
+*/
+ 
+Ext.define('Breeze.controller.Overseer', {
+    extend: 'Ext.app.Controller',
+
+    /*
+    models: [],
+    stores: [],
+
+    init: function() {
+
+    },
+s
+    onViewportRendered: function(params){
+        // Check if user is logged in, handle both possibilities
+    }
+    */
+
+ss});
---
+++ app/helper/Auth.js
@@ -0,0 +1,66 @@
+Ext.define('Breeze.app.helper.Auth', {
+
+    requires: [
+        'Breeze.app.helper.Cookie'
+    ],
+
+    statics: {
+        /**
+         * Create/update auth related cookies
+         */
+        setCookies: function(pass, cust, emp){
+            var c = Breeze.app.helper.Cookie;
+            c.bake('STOPASS', pass, 7);
+            c.bake('STOCUST', cust, 7);
+            c.bake('STOEMP', emp, 7);
+        },
+
+        /**
+         * Get 3 auth-related cookies
+         */
+        getCookies: function() {
+            var c = Breeze.app.helper.Cookie;
+            return {
+                pass:   c.get('STOPASS'),
+                cust:   c.get('STOCUST'),
+                emp:    c.get('STOEMP')
+            };
+        },
+
+        /**
+         * rewrite cookies with new expirations
+         */
+        reloadCookies: function(minutes){
+            var c = Breeze.app.helper.Cookie;
+            var old = this.getCookies();
+            c.bake('STOPASS', old.pass, minutes);
+            c.bake('STOCUST', old.cust, minutes);
+            c.bake('STOEMP', old.emp, minutes);
+        },
+
+        /**
+         * Check authorization cookies, optionally reloading if not okay
+         * @param {Boolean} forceReload if true, reload if invalid cookies (default false)
+         * @return {Boolean} True if authorized, false otherwise
+         */
+        isAuthorized: function(forceReload){
+            var cookies = Breeze.app.helper.Cookie;
+            var vals = this.getCookies();
+            var okay = (
+                vals.pass !== null &&
+                vals.cust !== null &&
+                vals.emp !== null
+            );
+            if(!okay && forceReload){
+                window.location.reload();
+                return false;
+            }
+            return okay;
+        }
+    },
+
+    authenticate: function(forceReload){
+        // TODO: Implement 'checkAuthentication' from 'homemade.js'
+    }
+
+});
