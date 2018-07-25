680f40f1281d52dd37061c88bc2edc4cbf7029c5
Jonathan Wszalek <wszalek@ventureinteractive.com>
2018-07-25 11:32:25 -0400

    TKO redefined analog clock

--- app/view/main/tko/AnalogClock.js
+++ app/view/main/tko/AnalogClock.js
@@ -1,22 +1,24 @@
 Ext.define('Breeze.view.main.tko.AnalogClock', {
-	alias:'widget.main.tko.analogClock',
-    extend: 'Ext.Container',
+    extend: 'Ext.Component',
+    alias:'widget.main.tko.analogClock',
 
-    requires:[
-    	'Breeze.view.main.tko.AnalogClockController'
-    ],
+    userCls:'tko-analog-clock',
+    html:"<div class='tko-analog-clock-min'></div><div class='tko-analog-clock-hrs'></div><div class='tko-analog-clock-sec'></div>",
 
-    controller:'main.tko.analogClock',
+    onRender:function(){
+        this.callParent(arguments);
+        this.updateTime();
+    },
 
-    items:[
-    	{
-    		xtype:'component',
-    		userCls:'tko-analog-clock',
-    		listeners:{
-    			initialize:'onInit'
-    		}
-    	}
-    ]
+    updateTime:function(){
+        var t = new Date();
+        var s = (t.getSeconds() + (t.getMilliseconds() / 1000)) * 6;
+        var m = (t.getMinutes() + (s / 360)) * 6;
+        var h = ((t.getHours() % 12) + (m / 360)) * 30;
+        this.el.select('.tko-analog-clock-min').elements[0].style.transform = 'rotateZ('+ m +'deg)';
+        this.el.select('.tko-analog-clock-hrs').elements[0].style.transform = 'rotateZ('+ h +'deg)';
+        this.el.select('.tko-analog-clock-sec').elements[0].style.transform = 'rotateZ('+ s +'deg)';
+    }
 
 
 });
