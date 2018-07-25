c508d374872d1ce042880fdd1df9c24456bdb9ad
Jonathan Wszalek <wszalek@ventureinteractive.com>
2018-07-25 17:19:47 -0400

    TKO-Clock Update, Update

--- app/view/main/tko/DigitalClock.js
+++ app/view/main/tko/DigitalClock.js
@@ -2,7 +2,8 @@ Ext.define('Breeze.view.main.tko.DigitalClock', {
 	extend:'Ext.Component',
 	alias:'widget.main.tko.digitalClock',
 	config:{
-		clockedIn:false
+		clockedIn:false,
+		abbreviated:false
 	},
 	userCls:'tko-digital-clock',
 	html:"<div class='tko-digital-clock-date'></div><div class='tko-digital-clock-time'></div><div class='tko-digital-clock-inout'></div>",
@@ -17,15 +18,26 @@ Ext.define('Breeze.view.main.tko.DigitalClock', {
 		if(this.rendered){
 			this.updateInout();
 		}
+		return ci;
 	},
+
+	applyAbbreviated:function(ab){
+		if(this.rendered){
+			this.updateDateTime();
+		}
+		return ab;
+	},
+
 
 	updateDateTime:function(){
 		var t = new Date();
 		var min = ("0" + (t.getMinutes())).slice(-2); 
 		var hrs = (t.getHours() + 24) % 12 || 12;
 		var mer = ["AM","PM"][Math.round(t.getHours()/24)];
-		var mon = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"][t.getMonth()];
-		//var mon = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"][t.getMonth()];
+		//var mon = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"][t.getMonth()];
+		//var mab = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"][t.getMonth()];
+		var mon = (this.getAbbreviated()) ? Ext.Date.getShortMonthName(t.getMonth()) : Ext.Date.monthNames[t.getMonth()];
+
 		var dat = t.getDate();
 		var ord = ["st","nd","rd"][((t.getDate()+90)%100-10)%10-1]||"th";
 		this.el.select('.tko-digital-clock-date').elements[0].innerHTML = mon + " " + dat + "<sup>" + ord + "</sup>";
@@ -38,7 +50,7 @@ Ext.define('Breeze.view.main.tko.DigitalClock', {
 
 	updateInout:function(){
 		console.log("[updateInout]");
-		console.log(this.config.clockedIn);
+		console.log(this.getClockedIn());
 		var io = ["&#10003; Clocked In", "&times; Clocked Out"][(this.config.clockedIn)? 0 : 1];
 		this.el.select('.tko-digital-clock-inout').elements[0].innerHTML = io;
 	}
