9e872dbf1889b18e7a248dc657ccf05807e17688
Jonathan Wszalek <wszalek@ventureinteractive.com>
2018-08-15 15:16:24 -0400

    Updates to calendar ui

--- app/view/employee/Calendar.js
+++ app/view/employee/Calendar.js
@@ -6,7 +6,10 @@ Ext.define('Breeze.view.employee.Calendar',{
     extend: 'Ext.Container',
     alias: 'widget.employee.calendar',
     requires: [
-        'Ext.calendar.panel.Month'
+        'Ext.calendar.panel.Month',
+        'Ext.Toolbar',
+        'Ext.Button',
+        'Ext.Component'
     ],
     
     //viewModel: {
@@ -21,19 +24,52 @@ Ext.define('Breeze.view.employee.Calendar',{
 
     userCls: 'employee-calendar-container',
 
-    items: [ 
+    items: [
+        {
+            xtype: 'toolbar',
+            docked: 'top',
+            ui: 'employee-calendar',
+            layout: {
+                type: 'hbox',
+                pack: 'center'
+            },
+            items: [
+                {
+                    xtype: 'button',
+                    ui: "calendarMonthSelectionButton",
+                    iconCls: 'x-fa fa-angle-left'
+                },
+                {
+                    xtype: 'component',
+                    html: 'Month Year'
+                },
+                {
+                    xtype: 'button',
+                    ui: "calendarMonthSelectionButton",
+                    iconCls: 'x-fa fa-angle-right'
+                }
+            ]
+        },
+
         {
             xtype: 'calendar',
+            userCls: 'employee-calendar',
             label: 'Month',
             flex: 1,
             ui: 'employee-calendar',
             compact: true,
+            titleBar: {
+                ui:'employee-calendar', 
+                shadow: false
+            },
             views:{
                 month:{
                     xtype: 'calendar-month',
+                    dayHeader: {
+                        ui:'employee-calendar',
+                    },
                     view:{
                         ui: 'employee-calendar',
- 
                     }
                 },
 
