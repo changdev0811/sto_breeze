8176f38b19fd731bd5d13ff3aa5e5bfb2a147e71
Wade Harkins <wade@ventureinteractive.com>
2018-09-14 13:52:45 -0400

    Removed `timely` lib and changed it to be made available by calling a method in `Time` helper

--- app/Application.js
+++ app/Application.js
@@ -43,7 +43,7 @@ Ext.define('Breeze.Application', {
 
     launch: function(){
         console.log("Launched");
-        
+        Breeze.helper.Time.loadTimely();
         // TODO: Removing testing check when out of dev
         var testing = true; // (should be removed later)
         if(testing){
