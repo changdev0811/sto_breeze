9c5e418886c67ca7bf0bd8cf38bf97193567f055
Wade Harkins <wade@ventureinteractive.com>
2018-09-17 18:20:43 -0400

    Custom `Toast` done, now with dismiss icon; Applied new toast to all existing calls in `NavController`

--- overrides/Toast.js
+++ overrides/Toast.js
@@ -19,7 +19,7 @@ Ext.define('Breeze.Toast', {
         // warning type constants
         WARN: 'warn', WARNING: 'warn',
         // info type constants
-        INFO: 'info', INFORMATIN: 'info'
+        INFO: 'info', INFORMATION: 'info', SUCCESS: 'info'
     },
 
     config: {
@@ -38,18 +38,20 @@ Ext.define('Breeze.Toast', {
         warnUi: 'toast-warn',
         /* ===[Icon Settings]=== */
         // Base icon style prefix
-        iconBaseClass: 'x-icon-el x-font-icon x-fas',
+        iconBaseClass: 'x-fas',
+        iconPadding: '2pt 4pt 2pt 4pt',
         // Error icon class
         // errorIcon: 'fa-times-circle',
         errorIcon: 'fa-times-octagon',
         // Informational icon class
-        // infoIcon: 'fa-info-circle',
-        infoIcon: 'fa-info',
+        infoIcon: 'fa-info-circle',
+        // infoIcon: 'fa-info',
         // Warning icon class
         // warnIcon: 'fa-exclamation-triangle',
         warnIcon: 'fa-exclamation',
         /* ===[Behavior]=== */
         // Whether toast is dismissable
+        dismissIcon: 'x-fa fa-times',
         dismissable: true
     },
 
@@ -62,22 +64,33 @@ Ext.define('Breeze.Toast', {
 
     applyMessage: function(value){
         var styling = this.typeStyles();
-        console.info('Styling: ', styling);
         var icon = this.generateIcon(styling.icon);
         var me = this;
+
         this.setUi(styling.ui);
+        
+        var messageItems = [
+            icon,
+            {
+                xtype: 'component',
+                html: value,
+                style: 'padding-right: 4pt',
+                flex: 1
+            },   
+        ];
+
+        if(this.getDismissable()){
+            messageItems.push(this.generateDismissButton());
+        }
+
         var msg = Ext.factory({
-            cls: me.baseCls + '-text',
-            ui: styling.ui,
-            layout: 'hbox',
-            items: [
-                icon,
-                {
-                    xtype: 'component',
-                    html: value,
-                    flex: 1
-                }
-            ]
+            cls: me.baseCls + '-text',            ui: styling.ui,
+            layout: {
+                type: 'hbox',
+                align: 'center'
+            },
+            // style: 'line-height: 14pt',
+            items: messageItems
         },Ext.Container,this._message);
         if(value && value !== null){
             return msg;
@@ -113,8 +126,18 @@ Ext.define('Breeze.Toast', {
         },
         generateIcon: function(icon){
             var iconClass = `${this.getIconBaseClass()} ${icon}`;
-            return Ext.create('Ext.Component', { html: `<span class="${iconClass}"></span>&nbsp;`});
+            return Ext.create('Ext.Component', { html: `<span class="${iconClass}" style="padding: ${this.getIconPadding()}"></span>&nbsp;`});
         },
+        generateDismissButton: function(){
+            var me = this;
+            return Ext.create('Ext.Button', {
+                iconCls: this.getDismissIcon(),
+                handler: function(){
+                    me.onTimeout();
+                }
+            });
+        }
+
     }
 
 });
