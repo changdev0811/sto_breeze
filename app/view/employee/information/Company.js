ea580d91117f1bf865f2fee718a0134c9350b92f
Jonathan Wszalek <wszalek@ventureinteractive.com>
2018-09-26 13:08:11 -0400

    Updated to night-time theme

--- app/view/employee/information/Company.js
+++ app/view/employee/information/Company.js
@@ -164,6 +164,7 @@ Ext.define('Breeze.view.employee.information.Company', {
                     },
                     reference: 'companyListTabs',
                     flex: 1,
+                    style:'background-color:#ff0000;',
                     items: [
                         // ===[Supervisors tab]==
                         {
