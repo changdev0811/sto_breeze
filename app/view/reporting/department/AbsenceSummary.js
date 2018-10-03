33a2560865dd2d32f9607b2b49e3a630fe735209
Shawn Thompson <shawn@ventureinteractive.com>
2018-10-03 07:56:20 -0400

    Added Reports > Department > Absence Summary files

---
+++ app/view/reporting/department/AbsenceSummary.js
@@ -0,0 +1,363 @@
+/**
+ * Department Absence Summary Report form
+ * @class AbsenceSummary
+ * @namespace Breeze.view.reporting.department.AbsenceSummary
+ * @alias widget.reporting.department.absencesummary
+ */
+Ext.define('Breeze.view.reporting.department.AbsenceSummary', {
+    extend: 'Ext.Panel',
+    alias: 'widget.reporting.department.absencesummary',
+
+    requires: [
+        'Ext.tab.Panel',
+        'Ext.list.Tree',
+        'Ext.form.FieldSet',
+        'Ext.field.Date',
+        'Ext.picker.Date',
+        'Ext.field.ComboBox',
+        'Ext.field.Spinner',
+        'Ext.field.Radio'
+    ],
+
+    // View Model
+
+    viewModel: {
+        type: 'reporting.department.absencesummary'
+    },
+    
+    // Controller
+
+    controller: 'reporting.department.absencesummary',
+
+    listeners: {
+        initialize: 'onInit'
+    },
+
+    // Layout and base styles
+    layout: 'vbox',
+    ui: 'reporting-base',
+
+    title: 'Department Absence Summary Report',
+
+    // Action buttons shown at bottom of panel
+    buttons: {
+        pdf: { text: 'PDF (Print)', handler: 'onPrintPDF', ui: 'action' },
+        excel: { text: 'Excel (Print)', handler: 'onPrintExcel', ui: 'action' },
+        word: { text: 'Word (Print)', handler: 'onPrintWord', ui: 'action' },
+    },
+
+    // Adjust action button toolbar spacing and appearance with UI and shadow
+    buttonToolbar: {
+        xtype: 'toolbar',
+        ui: 'reporting-actions',
+        shadow: false
+    },
+
+    // Body contents
+    items: [
+        // Form Title Text field
+        {
+            xtype: 'breeze-textfield',
+            label: 'Report Title',
+            name: 'reportTitle',
+            bind: '{reportParams.ReportTitle}',
+            ui: 'reporting reporting-text'
+        },
+        // Main horizontal arranging container
+        {
+            xtype: 'container',
+            flex: 1,
+            layout: 'hbox',
+            items: [
+                // First column in horizontal container
+                {
+                    xtype: 'container',
+                    // docked: 'left',
+                    flex: 1,
+                    layout: 'vbox',
+                    items: [
+                        // Tab panel containing departments and employees
+                        {
+                            xtype: 'tabpanel',
+                            ui: 'reporting-tabs',
+                            flex: 1,
+                            items: [
+                                // Departments tab
+                                {
+                                    xtype: 'panel',
+                                    title: 'Departments',
+                                    layout: 'fit',
+                                    // Toolbar containing 'check all' toggle checkbox
+                                    tbar: {
+                                        xtype: 'toolbar',
+                                        ui: 'reporting-tree',
+                                        items: [
+                                            {
+                                                xtype: 'checkbox',
+                                                boxLabel: 'Check All',
+                                                listeners: {
+                                                    change: 'onTreeGridCheckAllChange'
+                                                }
+                                            }
+                                        ]
+                                    },
+                                    items: [
+                                        // Departments tree
+                                        {
+                                            xtype: 'tree',
+                                            layout: 'hbox',
+                                            hideHeaders: true,
+                                            rootVisible: false,
+                                            columns: [
+                                                {
+                                                    xtype: 'checkcolumn',
+                                                    dataIndex: 'checked',
+                                                    minWidth: '2em',
+                                                    width: 'auto',
+                                                    padding: 0,
+                                                    listeners: {
+                                                        checkChange: 'onTreeGridChecked'
+                                                    }
+                                                },
+                                                {
+                                                    xtype: 'treecolumn',
+                                                    dataIndex: 'text',
+                                                    flex: 1,
+                                                    layout: {
+                                                        alignment: 'stretch'
+                                                    }
+                                                }
+                                            ],
+                                            // ui: 'reporting-tree',
+                                            reference: 'departmentTree',
+                                            bind: '{departmentsTree}'
+                                        }
+                                    ]
+                                },
+                                // Employees Tab
+                                {
+                                    xtype: 'panel',
+                                    title: 'Employees',
+                                    layout: 'fit',
+                                    // Toolbar containing 'check all' toggle checkbox
+                                    tbar: {
+                                        xtype: 'toolbar',
+                                        ui: 'reporting-tree',
+                                        items: [
+                                            {
+                                                xtype: 'checkbox',
+                                                boxLabel: 'Check All',
+                                                listeners: {
+                                                    change: 'onTreeGridCheckAllChange'
+                                                }
+                                            }
+                                        ]
+                                    },
+                                    items: [
+                                        // Employees selector tree
+                                        {
+                                            xtype: 'tree',
+                                            layout: 'hbox',
+                                            hideHeaders: true,
+                                            expanderFirst: false,
+                                            rootVisible: false,
+                                            columns: [
+                                                {
+                                                    xtype: 'checkcolumn',
+                                                    dataIndex: 'checked',
+                                                    minWidth: '2em',
+                                                    width: 'auto',
+                                                    padding: 0
+                                                },
+                                                {
+                                                    xtype: 'treecolumn',
+                                                    dataIndex: 'text',
+                                                    flex: 1,
+                                                    layout: {
+                                                        alignment: 'stretch'
+                                                    }
+                                                }
+                                            ],
+                                            // ui: 'reporting-tree',
+                                            reference: 'employeeTree',
+                                            bind: '{employeesTree}'
+                                        }
+                                    ]
+                                }
+                            ]
+                        },
+                        // Shared checkbox
+                        {
+                            xtype: 'checkbox',
+                            labelAlign: 'top',
+                            boxLabel: 'Group by Department',
+                            bodyAlign: 'stretch',
+                            ui: 'reporting'
+                        }
+                    ]
+                },
+                // Fieldset column container
+                {
+                    xtype: 'container',
+                    flex: 1,
+                    layout: 'vbox',
+                    defaults: {
+                        userCls: 'report-section-padding',
+                    },
+                    items: [
+                        {
+                            xtype: 'fieldset',
+                            layout: 'vbox',
+                            title: 'Header Options',
+                            userCls: 'report-section-padding reporting-fieldset',
+                            defaults: {
+                                bodyAlign: 'stretch',
+                                ui: 'reporting',
+                                xtype: 'breeze-checkbox'
+                            },
+                            items: [
+                                {
+                                    name: 'headerCompanyLogo',
+                                    inline: true,
+                                    label: '',
+                                    boxLabel: 'Company Logo in Header',
+                                    bind: '{reportParams.LogoInHeader}'
+                                },
+                                {
+                                    name: 'headerCompanyName',
+                                    label: '',
+                                    labelMinWidth: 0,
+                                    boxLabel: 'Company Name in Title',
+                                    bind: '{reportParams.NameInHeader}'
+                                },
+                                {
+                                    name: 'headerSignature',
+                                    label: '',
+                                    boxLabel: 'Signature Line in Footer',
+                                    bind: '{reportParams.RepSignature}'
+                                }
+                            ]
+                        },
+                        {
+                            xtype: 'fieldset',
+                            userCls: 'report-section-padding reporting-fieldset',
+                            title: 'Date Range',
+                            defaults: {
+                                ui: 'reporting reporting-text reporting-date'
+                            },
+                            items: [
+                                {
+                                    xtype: 'datefield',
+                                    name: 'start',
+                                    label: 'From',
+                                    picker: {
+                                        xtype: 'datepicker',
+                                        title: 'Start Date'
+                                    },
+                                    bind: '{reportParams.dStart}'
+                                },
+                                {
+                                    xtype: 'datefield',
+                                    name: 'finish',
+                                    label: 'To',
+                                    picker: {
+                                        xtype: 'datepicker',
+                                        title: 'End Date'
+                                    },
+                                    bind: '{reportParams.dEnd}'
+                                }
+                            ]
+                        },
+                        {
+                            xtype: 'fieldset',
+                            userCls: 'report-section-padding reporting-fieldset',
+                            title: 'Condition',
+                            items: [
+                                {
+                                    xtype: 'container',
+                                    reference: 'conditionValue',
+                                    layout: 'hbox',
+                                    defaults: {
+                                        ui: 'reporting reporting-text'
+                                    },
+                                    items: [
+                                        {
+                                            xtype: 'combobox',
+                                            name: 'conditionType',
+                                            flex: 2
+                                        },
+                                        {
+                                            xtype: 'spinnerfield',
+                                            name: 'conditionValue',
+                                            label: '',
+                                            flex: 1,
+                                            style: 'padding-left: 4pt'
+                                        }
+                                    ]
+                                },
+                                {
+                                    xtype: 'container',
+                                    reference: 'conditionType',
+                                    layout: 'hbox',
+                                    defaults: {
+                                        bodyAlign: 'stretch',
+                                        ui: 'reporting'
+                                    },
+                                    items: [
+                                        {
+                                            xtype: 'radio',
+                                            flex: 1,
+                                            name: 'conditionValueType',
+                                            value: '20',
+                                            boxLabel: 'Days',
+                                            bind: '{reportParams.conditional_type}'
+                                        },
+                                        {
+                                            xtype: 'radio',
+                                            flex: 1,
+                                            name: 'conditionValueType',
+                                            value: '21',
+                                            boxLabel: 'Weeks',
+                                            bind: '{reportParams.conditional_type}'
+                                            // bind: {
+                                            //     checked: '{reportOptions.conditionalValueType == 21}'
+                                            // }
+                                        }
+                                    ]
+                                }
+                            ]
+                        }
+                    ]
+                },
+                // Container for User-Defined Categories list
+                {
+                    xtype: 'container',
+                    // userCls: 'reporting-fieldset',
+                    // title: 'Categories',
+                    flex: 1,
+                    // docked: 'right',
+                    layout: {
+                        type: 'fit',
+                        alignment: 'stretch'
+                    },
+                    height: '100%',
+                    width: '100%',
+                    reference: 'udcContainer',
+                    items: [
+                        // User defined categories tree control
+                        {
+                            xtype: 'breeze.tree.usercategories',
+                            bind: {
+                                store: '{categoriesTree}'
+                            },
+                            reference: 'udcTree',
+                            flex: 1,
+                            ui: 'reporting-tree'
+                        }
+                    ]
+                }
+            ]
+        }
+    ]
+
+});
