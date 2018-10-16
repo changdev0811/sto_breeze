1a838ac9b335e56d37dbfe39d0edce7e99cf4189
Wade Harkins <wade@ventureinteractive.com>
2018-10-16 11:21:25 -0400

    Header comment added to Employees Panel View Controller class `PanelController`

--- app/view/main/employees/PanelController.js
+++ app/view/main/employees/PanelController.js
@@ -1,3 +1,10 @@
+/**
+ * View Controller for Employees Panel
+ * @class PanelController
+ * @namespace Breeze.view.main.employees.PanelController
+ * @extends Breeze.controller.Base
+ * @alias main.employees.panel
+ */
 Ext.define('Breeze.view.main.employees.PanelController', {
     extend: 'Breeze.controller.Base',
     alias: 'controller.main.employees.panel',
