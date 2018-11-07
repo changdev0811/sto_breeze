df71e3e56d744860feb96bdcd1b806406eb5ce7a
Jonathan Wszalek <wszalek@ventureinteractive.com>
2018-11-07 15:01:35 -0500

    Supervisor Roles

--- app/view/admin/Roles.js
+++ app/view/admin/Roles.js
@@ -26,222 +26,257 @@ Ext.define('Breeze.view.admin.Roles', {
 
     // Body contents
     items: [
-        // Column 1
+
+        // Main horizontal arranging container
         {
-            xtype: 'fieldset',
-            userCls:'admin-fieldset no-padding',
+            xtype: 'container',
             flex: 1,
-            layout: 'vbox',
-            items:[
+            layout: 'hbox',
+            // +++ Allow h scroll when panel is too small +++
+            scrollable:'x',
+            items: [
+
+                // Column 1
                 {
-                    xtype: 'toolbar',
-                    ui:'admin-tree',
-                    shadow: false,
+                    xtype: 'fieldset',
+                    userCls:'admin-fieldset no-padding',
+                    flex: 1,
+
+                    // +++ fixed width +++
+                    minWidth:'200pt',
+                    maxWidth:'200pt',
+
+                    layout: 'vbox',
                     items:[
-                        { 
-                            xtype: 'component', 
-                            html: 'Roles',
-                            userCls:'admin-title-toolbar', 
+                        {
+                            xtype: 'toolbar',
+                            ui:'admin-tree',
+                            shadow: false,
+                            items:[
+                                { 
+                                    xtype: 'component', 
+                                    html: 'Roles',
+                                    userCls:'admin-title-toolbar', 
+                                },
+                                {
+                                    xtype:'spacer',
+                                    flex:1,
+                                },
+                                {
+                                    xtype: 'button',
+                                    iconCls:'x-fas fa-plus',
+                                    ui: 'plain wtr-button',                   
+                                },
+                                {
+                                    xtype: 'button',
+                                    iconCls:'x-fas fa-minus',
+                                    ui: 'plain wtr-button',                   
+                                },
+                            ]
                         },
                         {
-                            xtype:'spacer',
+                            xtype: 'tree',
+                            // == Item ID to make finding tree in panel easier
+                            itemId: 'tree',
+                            ui: 'employeeinfo-shift-grid',
+                            userCls: 'employeeinfo-shift-grid no-border',
                             flex:1,
-                        },
-                        {
-                            xtype: 'button',
-                            iconCls:'x-fas fa-plus',
-                            ui: 'plain wtr-button',                   
-                        },
-                        {
-                            xtype: 'button',
-                            iconCls:'x-fas fa-minus',
-                            ui: 'plain wtr-button',                   
-                        },
+                            layout: 'hbox',
+                            hideHeaders: true,
+                            rootVisible: false,
+                            columns: [
+                                {
+                                    xtype: 'checkcolumn',
+                                    cell: {
+                                        ui: 'report-tree-column reporting-tree-item',
+                                    },
+                                    dataIndex: 'checked',
+                                    minWidth: '2em',
+                                    width: 'auto',
+                                    padding: 0,
+                                    //listeners: {
+                                    //    checkChange: 'onTreeGridChecked'
+                                    //}
+                                },
+                                {
+                                    xtype: 'treecolumn',
+                                    cell: {
+                                        ui: 'report-tree-column reporting-tree-item',
+                                    },
+                                    dataIndex: 'text',
+                                    flex: 1,
+                                    layout: {
+                                        alignment: 'stretch'
+                                    }
+                                }
+                            ],
+                            bind: '{departmentsTree}'
+                        }, 
                     ]
                 },
+                // Column 2
                 {
-                    xtype: 'tree',
-                    // == Item ID to make finding tree in panel easier
-                    itemId: 'tree',
-                    ui: 'employeeinfo-shift-grid',
-                    userCls: 'employeeinfo-shift-grid no-border',
-                    flex:1,
-                    layout: 'hbox',
-                    hideHeaders: true,
-                    rootVisible: false,
-                    columns: [
-                        {
-                            xtype: 'checkcolumn',
-                            cell: {
-                                ui: 'report-tree-column reporting-tree-item',
-                            },
-                            dataIndex: 'checked',
-                            minWidth: '2em',
-                            width: 'auto',
-                            padding: 0,
-                            //listeners: {
-                            //    checkChange: 'onTreeGridChecked'
-                            //}
-                        },
-                        {
-                            xtype: 'treecolumn',
-                            cell: {
-                                ui: 'report-tree-column reporting-tree-item',
-                            },
-                            dataIndex: 'text',
-                            flex: 1,
-                            layout: {
-                                alignment: 'stretch'
-                            }
-                        }
-                    ],
-                    bind: '{departmentsTree}'
-                }, 
-            ]
-        },
-        // Column 2
-        {
-            xtype: 'panel',
-            ui: 'admin-sub',
-            userCls:'admin-fieldset no-border no-padding',
-            flex: 2,
-            layout: 'vbox',
-            buttons: {
-                apply: { text: 'Apply', /*handler: 'onPrintPDF',*/ ui: 'action' },
-            },
-            buttonToolbar: {
-                xtype: 'toolbar',
-                ui: 'admin-actions',
-                shadow: false
-            },
-            items:[
-               
-                {
-                    xtype: 'breeze-textfield',
-                    label: 'Role Name',
-                    name: 'project_name',
-                    ui: 'admin admin-text',
+                    xtype: 'panel',
+                    ui: 'admin-sub',
                     userCls:'admin-fieldset no-border',
-                },
-                {
-                    xtype:'fieldset',
-                    userCls:'admin-fieldset',
-                    title:'Supervisor Rights',
+                    flex: 2,
+
+                    // +++ fixed width +++
+                    minWidth:'200pt',
+                    maxWidth:'400pt',
+
                     layout: 'vbox',
-                    defaults:{
-                        userCls:'admin-fieldset no-border no-padding',
+                    buttons: {
+                        apply: { text: 'Apply', /*handler: 'onPrintPDF',*/ ui: 'action', style:'width:175pt;' },
+                    },
+                    buttonToolbar: {
+                        xtype: 'toolbar',
+                        ui: 'admin-actions',
+                        shadow: false
                     },
                     items:[
+                       
                         {
-                            xtype:'checkbox',
-                            ui:'admin',
-                            name: 'isWorktime',
-                            id: 'radio1',
-                            value: '20',
-                            boxLabel: 'Check All',
-                            bodyAlign: 'stretch',
+                            xtype:'fieldset',
+                            ui: 'admin admin-text',
+                            userCls:'admin-fieldset ',
+                            layout:'vbox',
+                            items:[
+                                {
+                                    xtype: 'breeze-textfield',
+                                    label: 'Role Name',
+                                    name: 'project_name',
+                                    ui: 'admin admin-text',
+                                    userCls:'admin-fieldset no-border no-side-margin',
+                                },
+                            ]
                         },
 
                         {
-                            xtype:'checkbox',
-                            ui:'admin',
-                            name: 'isOT',
-                            id: 'radio2',
-                            value: '20',
-                            boxLabel: 'Add Employee',
-                            bodyAlign: 'stretch',
-                        },
-                        {
-                            xtype:'checkbox',
-                            ui:'admin',
-                            name: 'isOT',
-                            id: 'radio3',
-                            value: '20',
-                            boxLabel: 'Delete Employee',
-                            bodyAlign: 'stretch',
-                        },
-                        {
-                            xtype:'checkbox',
-                            ui:'admin',
-                            name: 'isOT',
-                            id: 'radio4',
-                            value: '20',
-                            boxLabel: 'Edit Employee',
-                            bodyAlign: 'stretch',
-                        },
-                        {
-                            xtype:'checkbox',
-                            ui:'admin',
-                            name: 'isOT',
-                            id: 'radio5',
-                            value: '20',
-                            boxLabel: 'View SSN',
-                            bodyAlign: 'stretch',
-                        },
-                        {
-                            xtype:'checkbox',
-                            ui:'admin',
-                            name: 'isOT',
-                            id: 'radio6',
-                            value: '20',
-                            boxLabel: 'View Compensation',
-                            bodyAlign: 'stretch',
-                        },
-                        {
-                            xtype:'checkbox',
-                            ui:'admin',
-                            name: 'isOT',
-                            id: 'radio7',
-                            value: '20',
-                            boxLabel: 'Employee Category Adjust',
-                            bodyAlign: 'stretch',
-                        },
-                        {
-                            xtype:'checkbox',
-                            ui:'admin',
-                            name: 'isOT',
-                            id: 'radio8',
-                            value: '20',
-                            boxLabel: 'Adjustments',
-                            bodyAlign: 'stretch',
-                        },
-                        {
-                            xtype:'checkbox',
-                            ui:'admin',
-                            name: 'isOT',
-                            id: 'radio9',
-                            value: '20',
-                            boxLabel: 'Leave Approvial',
-                            bodyAlign: 'stretch',
-                        },
-                        {
-                            xtype:'checkbox',
-                            ui:'admin',
-                            name: 'isOT',
-                            id: 'radio10',
-                            value: '20',
-                            boxLabel: 'Modify Recorded Time',
-                            bodyAlign: 'stretch',
-                        },
+                            xtype:'fieldset',
+                            userCls:'admin-fieldset',
+                            title:'Supervisor Rights',
+                            layout: 'vbox',
+                            scrollable:'y',
+                            flex:1,
+                            defaults:{
+                                userCls:'admin-fieldset no-border no-padding',
+                            },
+                            items:[
+                                {
+                                    xtype:'checkbox',
+                                    ui:'admin',
+                                    name: 'isWorktime',
+                                    id: 'radio1',
+                                    value: '20',
+                                    boxLabel: 'Check All',
+                                    bodyAlign: 'stretch',
+                                },
+
+                                {
+                                    xtype:'checkbox',
+                                    ui:'admin',
+                                    name: 'isOT',
+                                    id: 'radio2',
+                                    value: '20',
+                                    boxLabel: 'Add Employee',
+                                    bodyAlign: 'stretch',
+                                },
+                                {
+                                    xtype:'checkbox',
+                                    ui:'admin',
+                                    name: 'isOT',
+                                    id: 'radio3',
+                                    value: '20',
+                                    boxLabel: 'Delete Employee',
+                                    bodyAlign: 'stretch',
+                                },
+                                {
+                                    xtype:'checkbox',
+                                    ui:'admin',
+                                    name: 'isOT',
+                                    id: 'radio4',
+                                    value: '20',
+                                    boxLabel: 'Edit Employee',
+                                    bodyAlign: 'stretch',
+                                },
+                                {
+                                    xtype:'checkbox',
+                                    ui:'admin',
+                                    name: 'isOT',
+                                    id: 'radio5',
+                                    value: '20',
+                                    boxLabel: 'View SSN',
+                                    bodyAlign: 'stretch',
+                                },
+                                {
+                                    xtype:'checkbox',
+                                    ui:'admin',
+                                    name: 'isOT',
+                                    id: 'radio6',
+                                    value: '20',
+                                    boxLabel: 'View Compensation',
+                                    bodyAlign: 'stretch',
+                                },
+                                {
+                                    xtype:'checkbox',
+                                    ui:'admin',
+                                    name: 'isOT',
+                                    id: 'radio7',
+                                    value: '20',
+                                    boxLabel: 'Employee Category Adjust',
+                                    bodyAlign: 'stretch',
+                                },
+                                {
+                                    xtype:'checkbox',
+                                    ui:'admin',
+                                    name: 'isOT',
+                                    id: 'radio8',
+                                    value: '20',
+                                    boxLabel: 'Adjustments',
+                                    bodyAlign: 'stretch',
+                                },
+                                {
+                                    xtype:'checkbox',
+                                    ui:'admin',
+                                    name: 'isOT',
+                                    id: 'radio9',
+                                    value: '20',
+                                    boxLabel: 'Leave Approvial',
+                                    bodyAlign: 'stretch',
+                                },
+                                {
+                                    xtype:'checkbox',
+                                    ui:'admin',
+                                    name: 'isOT',
+                                    id: 'radio10',
+                                    value: '20',
+                                    boxLabel: 'Modify Recorded Time',
+                                    bodyAlign: 'stretch',
+                                },
+
+                            ]
+
+                        }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
                     ]
-
-                }
-
-
-
-
-
-
-
-
-
-
-
-
-
+                },
 
             ]
-        },
+        }
+
     ]
 });
