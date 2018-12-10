22e17d39b7eadd9102dfb06742d99c075c631ad1
Wade Harkins <wade@ventureinteractive.com>
2018-12-10 10:57:45 -0500

    API Class for admin > point categories added, along with store and data model items pertaining to said view

---
+++ app/api/admin/PointCats.js
@@ -0,0 +1,33 @@
+/**
+ * Admin Point Categories API calls
+ * @class PointCats
+ * @namespace Breeze.api.admin.PointCats
+ * @extends Breeze.api.Base
+ */
+Ext.define('Breeze.api.admin.PointCats', {
+   extend: 'Breeze.api.Base',
+
+   /**
+    * TODO: Implement add
+    * @api addPointCategory
+    */
+   add: function(){
+
+   },
+
+   /**
+    * TODO: Implement delete
+    * @api deletePointCategory
+    */
+   delete: function(){
+
+   },
+
+   /**
+    * TODO: implement update
+    * @api updatePointCategory
+    */
+   update: function(){
+
+   }
+});
---
+++ app/store/point/CategoryList.js
@@ -0,0 +1,43 @@
+/**
+ * Point Cateogory > UDC List (updated api)
+ * @class CategpryList
+ * @namespace Breeze.store.point.CategoryList
+ * @alias store.point.udclist
+ * @extends Breeze.store.Base
+ * @api getUDCPointsListAPI
+ */
+Ext.define('Breeze.store.point.CategoryList', {
+    extend: 'Breeze.store.Base',
+    model: 'Breeze.model.data.InfoObj',
+    alias: 'store.point.list',
+    autoLoad: false,
+    config: {
+        pointID: null
+    },
+    listeners: {
+        beforeload: function () {
+            this.provideAuthCookieToProxy();
+            this.useJsonParams();
+            this.getProxy().extraParams.pointID = this.getPointID();
+        }
+    },
+    proxy: {
+        type: 'ajax', // Because it's a cross-domain request
+        url: Breeze.helper.Store.api.url('getUDCPointsListAPI'),
+        headers: { 'Content-Type': 'application/json;' },
+        actionMethods: {
+            create: 'POST',
+            read: 'POST',
+            update: 'POST',
+            destroy: 'POST'
+        },
+        reader: {
+            type: 'json',
+            rootProperty: 'd' // The returned JSON will have array
+            // of users under a "users" property
+        },
+        // Don't want proxy to include these params in request
+        pageParam: undefined,
+        startParam: undefined
+    }
+});
