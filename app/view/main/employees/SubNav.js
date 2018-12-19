24910f7387413d1bd6e6b8b1dca51df951759d32
Jonathan Wszalek <wszalek@ventureinteractive.com>
2018-12-19 11:15:15 -0500

    review updates

--- app/view/main/employees/SubNav.js
+++ app/view/main/employees/SubNav.js
@@ -17,40 +17,89 @@ Ext.define('Breeze.view.main.employees.SubNav', {
     items: [
 
       {
-          xtype: 'segmentedbutton',
+          //xtype: 'segmentedbutton',
+          xtype: 'container',
+          layout:'hbox',
+          flex:1,
           defaults: {
-            margin:'20pt 0pt 4pt 22pt',
+            //margin:'20pt 0pt 4pt 22pt',
           },
           items: [
               {
                   xtype: 'button',
                   ui:'sub-nav-button calIcon',
+                  itemId:'cal',
                   iconCls: 'x-fas fa-calendar calIcon',
-                  text: 'Calendar'
+                  margin:'20pt 0pt 4pt 22pt',
+                  enableToggle:true,
+                  text: 'Calendar',
+                  listeners:{
+                    tap:'onNavTap'
+                  }
+              },
+              {
+                  xtype:'container',
+                  flex:1,
               },
               {
                   xtype: 'button',
                   ui:'sub-nav-button empInfIcon',
+                  itemId:'empinfo',
                   iconCls: 'x-fas fa-id-card empInfIcon',
-                  text: 'Employee Information'
+                  margin:'20pt 11pt 4pt 11pt',
+                  enableToggle:true,
+                  text: 'Employee Information',
+                  listeners:{
+                    tap:'onNavTap'
+                  }
+              },
+              {
+                  xtype:'container',
+                  flex:1,
               },
               {
                   xtype: 'button',
                   ui:'sub-nav-button fyiIcon',
+                  itemId:'fyi',
                   iconCls: 'x-fas fa-table fyiIcon',
-                  text: 'FYI'
+                  margin:'20pt 11pt 4pt 11pt',
+                  enableToggle:true,
+                  text: 'FYI',
+                  listeners:{
+                    tap:'onNavTap'
+                  }
+              },
+              {
+                  xtype:'container',
+                  flex:1,
               },
               {
                   xtype: 'button',
                   ui:'sub-nav-button yearAtGlanceIcon',
+                  itemId:'yag',
                   iconCls: 'x-fas fa-eye yearAtGlanceIcon',
-                  text: 'Year at a Glance'
+                  margin:'20pt 11pt 4pt 11pt',
+                  enableToggle:true,
+                  text: 'Year at a Glance',
+                  listeners:{
+                    tap:'onNavTap'
+                  }
+              },
+              {
+                  xtype:'container',
+                  flex:1,
               },
               {
                   xtype: 'button',
                   ui:'sub-nav-button worktimeRecIcon',
+                  itemId:'wtv',
                   iconCls: 'x-fas fa-calendar-check-o worktimeRecIcon',
-                  text: 'Worktime Records'
+                  margin:'20pt 22pt 4pt 11pt',
+                  enableToggle:true,
+                  text: 'Worktime Records',
+                  listeners:{
+                    tap:'onNavTap'
+                  }
               }
           ]
       }
---
+++ app/view/main/employees/SubNavController.js
@@ -0,0 +1,29 @@
+/**
+ * View Controller for Employees Panel
+ * @class PanelController
+ * @namespace Breeze.view.main.employees.PanelController
+ * @extends Breeze.controller.Base
+ * @alias main.employees.panel
+ */
+Ext.define('Breeze.view.main.employees.SubNavController', {
+    extend: 'Breeze.controller.Base',
+    alias: 'controller.main.employees.subnav',
+
+    /**
+     * Called when the view is created
+     */
+    onInit: function () {
+
+
+    },
+
+
+    onNavTap: function(bp){
+        //setPressed:true/false
+
+        var b_arr = [];
+
+
+    }
+   
+});
