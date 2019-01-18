bffdb9ffa987fe8e15103a321e2adb406f99d419
jsguru-git <js.guru@hotmail.com>
2019-01-19 06:28:11 +1000

    In Reports > Employee Reports > Employee Time Activity Report, Categories section validation error and value issue are fixed.

--- app/view/reporting/employee/TimeActivityController.js
+++ app/view/reporting/employee/TimeActivityController.js
@@ -81,7 +81,7 @@ Ext.define('Breeze.view.reporting.employee.TimeActivityController', {
             messages.push('Please select a Department or Employee.');
         }
 
-        if(vmData.reportParams.inccats == null){
+        if(vmData.reportParams.inccats == ''){
             valid = false;
             messages.push('Please select a Category.')
         }
@@ -120,14 +120,14 @@ Ext.define('Breeze.view.reporting.employee.TimeActivityController', {
         // Categories list method gatherSelected returns array of all records selected
         var categoryRecords = categoryList.gatherSelected(),
             // set selected category to the first selected record, if any, otherwise null
-            selectedCategory = (categoryRecords.length > 0)? categoryRecords[0] : null;
+            // selectedCategory = (categoryRecords.length > 0)? categoryRecords[0] : null;
             // get array of selected categories, using map to filter out the IDs
-            selectedCategories = categoryRecords.map((r)=>{r.getData().Category_Id});
-            // assign list of category ids as single string, joined with ','
-            vm.set(
-                'reportParams.inccats',
-                selectedCategories.join(',')
-            );
+            selectedCategories = categoryRecords.map((r)=>r.getData().Category_Id);
+        // assign list of category ids as single string, joined with ','
+        vm.set(
+            'reportParams.inccats',
+            selectedCategories.join(',')
+        );
     },
 
     /**
