f57750f312b7560f5e34378db70d5873304c709a
jsguru-git <js.guru@hotmail.com>
2019-01-22 23:31:30 +1000

    In Reports > Employee Reports > Worktime Details, companyConfig logic implemented in Controller and Model

--- app/view/reporting/employee/WorktimeDetailsModel.js
+++ app/view/reporting/employee/WorktimeDetailsModel.js
@@ -19,9 +19,11 @@ Ext.define('Breeze.view.reporting.employee.WorktimeDetailsModel', {
         var data = {
             reportParams: {
                 GroupByDept: true,
-                LogoInHeader: false,
-                NameInHeader: false,
-                RepSignature: false,
+                LogoInHeader: false,    // reset by RepLogo of companyConfig store
+                NameInHeader: false,    // reset by RepComp of companyConfig store
+                RepSignature: false,    // reset by RepSignature of companyConfig store
+                CompanyName: '',        // reset by CompanyName of companyConfig store
+                RepLogoPath: '',        // reset by RepLogoPath of companyConfig store
                 ReportTitle: 'Employee Worktime Details Report',
                 dStart: (new Date()),
                 dEnd: (new Date()),
