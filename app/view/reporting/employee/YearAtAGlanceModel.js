cabd7dc84cfaa7f6a732bd4edc9ce7854153543d
jsguru-git <js.guru@hotmail.com>
2019-01-22 23:33:28 +1000

    In Reports > Employee Reports > Year At A Glance, companyConfig logic implemented in Controller and Model

--- app/view/reporting/employee/YearAtAGlanceModel.js
+++ app/view/reporting/employee/YearAtAGlanceModel.js
@@ -19,9 +19,11 @@ Ext.define('Breeze.view.reporting.employee.YearAtAGlanceModel', {
         var data = {
             reportParams: {
                 GroupByDept: true,
-                LogoInHeader: false,
-                NameInHeader: false,
-                RepSignature: false,
+                LogoInHeader: false,    // reset by RepLogo of companyConfig store
+                NameInHeader: false,    // reset by RepComp of companyConfig store
+                RepSignature: false,    // reset by RepSignature of companyConfig store
+                CompanyName: '',        // reset by CompanyName of companyConfig store
+                RepLogoPath: '',        // reset by RepLogoPath of companyConfig store
                 ReportTitle: 'Employee Year At A Glance Report',
                 // Concerning selected departments/ids
                 idtype: 'emps', // According to existing code, this is always 'emps'
