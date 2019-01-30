6afe7131bcc67406bb31b3465feb82c84cbc9ca6
Wade Harkins <wade@ventureinteractive.com>
2019-01-30 17:41:25 -0500

    Leave request style class config option added to `EventBase` override class

--- overrides/calendar/EventBase.js
+++ overrides/calendar/EventBase.js
@@ -4,84 +4,21 @@
  */
 Ext.define('Breeze.overrides.calendar.EventBase', {
     override: 'Ext.calendar.EventBase',
+    /**
+     * Extra config params, see original EventBase of others
+     */
     config: {
-        /**
-         * @cfg {String} defaultTitle
-         * The default title to use when one is not specified.
-         * @locale
-         */
-        defaultTitle: '(New Event)',
-
-        /**
-         * @cfg {Date} endDate
-         * The end date for this event (as UTC). Will be set automatically if
-         * a {@link #model} is passed. May be set independently
-         * of any attached {@link #model}.
-         */
-        endDate: null,
-
-        /**
-         * @cfg {String} mode
-         * The display mode for this event. Possible options are:
-         * - `weekspan`
-         * - `weekinline`
-         * - `day`
-         */
-        mode: null,
-
-        /**
-         * @cfg {Ext.calendar.model.EventBase} model
-         * A backing model for this widget.
-         */
-        model: null,
-
-        /**
-         * @cfg {Ext.calendar.theme.Palette} palette
-         * A color palette for this event.
-         */
-        palette: null,
-
-        /**
-         * @cfg {Boolean} resize
-         * `true` to allow this event to be resized via the UI.
-         */
-        resize: false,
-
-        /**
-         * @cfg {Date} startDate
-         * The start date for this event (as UTC). Will be set automatically if
-         * a {@link #model} is passed. May be set independently
-         * of any attached {@link #model}.
-         */
-        startDate: null,
-
-        /**
-         * @cfg {String} title
-         * The title for this event. Will be set automatically if
-         * a {@link #model} is passed.
-         */
-        title: '',
-
-        /**
-         * @cfg touchAction
-         * @inheritdoc Ext.Widget#cfg-touchAction
-         */
-        touchAction: {
-            panX: false,
-            panY: false
-        },
-
-        /**
-         * @cfg {Ext.calendar.view.Base} view
-         * The view for this event.
-         */
-        view: null,
-
         /**
          * @cfg {Boolean} leave
          * Used to represent whether event is a leave request
          */
-        leave: false
+        leave: false,
+
+        /**
+         * @cfg {String} leaveStyleClass
+         * Style class to apply to leave requests
+         */
+        leaveStyleClass: ''
     },
     updateModel: function (model) {
         var me = this,
