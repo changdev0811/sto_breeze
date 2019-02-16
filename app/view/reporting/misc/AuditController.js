504fae02f9a14858eed1708fb2da69630a5feb4b
jsguru-git <js.guru@hotmail.com>
2019-02-17 01:31:44 +1000

    In Reports->Miscellaneous Reports, Audit Report completed

--- app/view/reporting/misc/AuditController.js
+++ app/view/reporting/misc/AuditController.js
@@ -28,32 +28,32 @@ Ext.define('Breeze.view.reporting.misc.AuditController', {
             {exceptionHandler: this.onReportException}
         );
 
-        // Load User-Defined Categories tree store
-        // this.addStoreToViewModel(
-        //     'Breeze.store.category.List',
-        //     'categoriesList',
-        //     { load: true }
-        // );
-
-        // Load employees for tree selector
-        // this.addStoreToViewModel(
-        //     'Breeze.store.tree.reporting.Employees',
-        //     'employeesTree',
-        //     { load: true }
-        // );
-
-        // Load departments for tree selector
-        // this.addStoreToViewModel(
-        //     'Breeze.store.tree.reporting.Departments',
-        //     'departmentsTree',
-        //     { load: true }
-        // );
-
         // Load company config
         this.addStoreToViewModel(
             'Breeze.store.company.Config',
             'companyConfig',
-            { load: true }
+            { 
+                load: true,
+                // callback to store Company configs
+                loadOpts: { callback: (success) => {
+                    if(success){
+                        let config = vm.get('companyConfig'),
+                            companyParams = config.getAt(0);
+                        vm.set(
+                            'reportParams.LogoInHeader', 
+                            companyParams.get('RepLogo')
+                        );
+                        vm.set(
+                            'reportParams.NameInHeader',
+                            companyParams.get('RepComp')
+                        );
+                        vm.set(
+                            'reportParams.RepSignature',
+                            companyParams.get('RepSignature')
+                        );
+                    }
+                }}
+            }
         );
 
         console.info('Leaving init');
--- app/view/reporting/misc/AuditModel.js
+++ app/view/reporting/misc/AuditModel.js
@@ -18,7 +18,6 @@ Ext.define('Breeze.view.reporting.misc.AuditModel', {
          */
         var data = {
             reportParams: {
-                GroupByDept: true,
                 LogoInHeader: false,
                 NameInHeader: false,
                 RepSignature: false,
